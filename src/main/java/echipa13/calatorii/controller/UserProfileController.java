8c28008402e4200194bfa6bbc047b4be0c01f6c3
Denisa <denisagheorghe18@gmail.com>
2025-12-11 02:52:02 +0200

    ghidul poate adauga tururi si user_profile page actualizata

--- src/main/java/echipa13/calatorii/controller/UserProfileController.java
+++ src/main/java/echipa13/calatorii/controller/UserProfileController.java
@@ -2,11 +2,16 @@ package echipa13.calatorii.controller;
 
 import echipa13.calatorii.Dto.LoginDto;
 import echipa13.calatorii.Dto.RegistrationDto;
+import echipa13.calatorii.Dto.TourDto;
 import echipa13.calatorii.Dto.registerDto;
+import echipa13.calatorii.models.Guide;
 import echipa13.calatorii.models.Role;
 import echipa13.calatorii.models.UserEntity;
+import echipa13.calatorii.repository.GuideRepository;
 import echipa13.calatorii.repository.RoleRepository;
 import echipa13.calatorii.repository.UserRepository;
+import echipa13.calatorii.service.GuideService;
+import echipa13.calatorii.service.TourService;
 import echipa13.calatorii.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
@@ -20,6 +25,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 import java.util.Collections;
 
@@ -28,6 +34,17 @@ public class UserProfileController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private GuideRepository guideRepository;
+
+    @Autowired
+    private GuideService guideService;
+    @Autowired
+    private TourService tourService;
 
     @GetMapping("/user_profile")
     public String userProfile(Model model, Authentication authentication) {
@@ -40,13 +57,27 @@ public class UserProfileController {
         String username = authentication.getName();
         System.out.println("Logged in username: " + username);
 
-        UserEntity user = userService.findByEmail(username);
+        UserEntity user = userService.findByUsername(username);
         if (user == null) {
             System.out.println("User not found in DB!");
             return "redirect:/login";
         }
 
         model.addAttribute("user", user);
+
+        Guide guide = guideRepository.findByUser_Id(user.getId()).orElse(null);
+
+        List<TourDto> calatorii;
+        if (guide != null) {
+            calatorii = tourService.findByGuideId(guide.getId());
+        } else {
+            calatorii = Collections.emptyList();
+        }
+
+        model.addAttribute("calatorii", calatorii);
+        model.addAttribute("isGuide", guide != null);
+
+
         return "user_profile";
     }
 
